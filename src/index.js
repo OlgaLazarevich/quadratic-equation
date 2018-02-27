d68c2ff4dc63d00942cf17c7dc20576008d9d536
Olga Lazarevich <helgalazarevich@gmail.com>
2018-02-27 17:35:56 +0200

    Quadratic equation: first solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var arr = equation.split(' ');
+
+    let a = arr[0],
+        c, b;
+
+    arr[3] == '-' ? b = '-' + arr[4] : b = arr[4];
+
+    arr[7] == '-' ? c = '-' + arr[8] : c = arr[8];
+
+    var d = Math.pow(b, 2) - 4 * a * c;
+
+    var first = (-(b) + Math.sqrt(d)) / (2 * a);
+    var second = (-(b) - Math.sqrt(d)) / (2 * a);
+
+    let solutions = [first, second];
+
+
+
+    return solutions
 }
